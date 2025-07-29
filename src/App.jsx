937452d82f99cb84cf5fa92bd2de14ef165faa0a
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-07-29 22:47:27 +0600

    Class-based component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-
+import "./App.css";
+import ClassBasedComponent from "./components/class-based-component";
 function App() {
   
 
   return (
       <div>
         <h1> React JS Concepts</h1>
+        <ClassBasedComponent/>
       </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/class-based-component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class ClassBasedComponent extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Class Based component:- class based components are now used less in modern React development.</h4>
+            </div>
+        );
+    }
+}
+export default ClassBasedComponent;
