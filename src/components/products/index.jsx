05888bb2376fd2eb172010419460b5c13a19e48e
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-07-31 21:30:34 +0600

    List and keys

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -4,7 +4,16 @@ const dummyProductData = ["Product 1", "Produc 2"];
 
 function ProductList(){
      return <div> <h4> ECommerece Project</h4> 
-     <ProductItem/>
+     {/* <ProductItem/> */}
+
+     <ul>
+          {
+               dummyProductData.map((item,index)=>(
+                    <li key={index}>{item}</li>
+               ))
+          }
+     </ul>
+
      </div>
 }
 export default ProductList;
