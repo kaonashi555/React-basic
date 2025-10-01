26d0ae840b12f685d386b7c4b581908567a06a47
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-10-01 13:53:37 +0600

    componentDidUpate Example

--- src/components/class-based-component.jsx
+++ src/components/class-based-component.jsx
@@ -6,6 +6,8 @@ class ClassBasedComponent extends Component{
     state = {
         showText : false,
         changeColor : false,
+        count: 0,
+        changeCountStyle: false,
     };
 
 
@@ -25,26 +27,49 @@ class ClassBasedComponent extends Component{
 
         this.setState({
             showText: !this.state.showText,
-            changeColor: !this.state,
+            changeColor: !this.state.changeColor,
         });
     }
+
+      handleCount = () => {
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    });
+  };
 
     //componentDidUpate
     // componentDidUpdate(prevProps,prevState){
     //     console.log(prevProps,prevState);
     // }
     componentDidUpdate(prevProps,prevState){
-        console.log(prevState, this.state);
+        if ( prevState &&prevState.count !== this.state.count &&this.state.count === 10
+        ) {
+            this.setState({
+            ...this.state,
+            changeCountStyle: true,
+            });
+        }
     }
+
 
     
     render(){
-
-        const{showText,changeColor}=this.state
+        const{showText,changeColor, count, changeCountStyle }=this.state
+        console.log(changeCountStyle);
         return(
             <div>
-                {showText? <h4 style={{color:changeColor? "red": "black"}}>Class Based component:- class based components are now used less in modern React development.</h4> : null}
+                {showText? <h4 style={{color:changeColor? "red": "black"}}>abcdefghijklmnopqrstuvwxyz</h4> : null}
                 <button onClick={this.handleClick}>Toggle Text</button>
+                <button onClick={this.handleCount}>Increase Count Value</button>
+        <h3
+          style={{
+            color: changeCountStyle ? "blue" : "yellow",
+            fontSize: changeCountStyle ? "30px" : "12px",
+          }}
+        >
+          Count is {count}
+        </h3>
             </div>
         );
     }
